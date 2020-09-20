2e1e671d3588ea68ff412fe0940830d964078718
Robert DeAngelis <deangelis.robert13@gmail.com>
2020-09-20 00:52:56 -0400

    bug

--- wayTooLongWords/src/Main.cpp
+++ wayTooLongWords/src/Main.cpp
@@ -1,11 +1,36 @@
 #include <bits/stdc++.h>
+#include <vector>
+
+std::vector<std::string> shortenString(std::vector<std::string> strings)
+{
+    std::vector<std::string> finalString;
+    for(int i = 0; i < static_cast<int>(strings.size()); i++)
+    {
+        if(strings[i].length() > 10)
+        {
+            std::string num{strings[i].length()-2};
+            std::string tmp{strings[i][0] + num + strings[i][strings[i].length()]};
+            finalString.push_back(tmp);
+        }
+        else
+            finalString.push_back(strings[i]);
+    }
+
+    return finalString;
+}
 
 int main()
 {
     int x{};
-    std::cin << x;
+    std::cin >> x;
+    std::vector<std::string> words;
+    std::string tmp;
     for(int i = 0; i < x; i++)
     {
-
+        std::cin >> tmp;
+        words.push_back(tmp);
     }
+    std::vector<std::string> shortStr{shortenString(words)};
+    for(int i = 0; i < x; i++)
+        std::cout << shortStr[i] << '\n';
 }
