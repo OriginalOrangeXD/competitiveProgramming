e76b5c83428e1dae62d2b978702294d952006170
Robert DeAngelis <deangelis.robert13@gmail.com>
2020-09-24 22:24:13 -0400

    nextRound

--- wayTooLongWords/src/Main.cpp
+++ wayTooLongWords/src/Main.cpp
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+#include <string>
 #include <vector>
 
 std::vector<std::string> shortenString(std::vector<std::string> strings)
@@ -8,9 +9,8 @@ std::vector<std::string> shortenString(std::vector<std::string> strings)
     {
         if(strings[i].length() > 10)
         {
-            std::string num{strings[i].length()-2};
-            std::string tmp{strings[i][0] + num + strings[i][strings[i].length()]};
-            finalString.push_back(tmp);
+            int a{(int)strings[i].length()-2};
+            finalString.push_back(strings[i][0] + std::to_string(a) + strings[i][strings[i].length()-1]);
         }
         else
             finalString.push_back(strings[i]);
